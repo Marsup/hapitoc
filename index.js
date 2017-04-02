aa4ed8ddd58b11f88619eaf05cfd27b477000738
Nicolas Morel <nicolas@morel.io>
2017-04-02 22:21:22 +0200

    Strip html tags

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ internals.generateToc = function () {
         slugify(text) {
 
             return text.toLowerCase()
+                .replace(/<\/?[^>]+(>|$)/g, '')
                 .replace(/\s/g, '-')
                 .replace(/[^\w-]/g, '');
         }
